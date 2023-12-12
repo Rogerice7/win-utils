a3489bc7efd612b3d26061e26021b5599e04ad3b
Rogerice7 <117949307+Rogerice7@users.noreply.github.com>
2023-12-12 21:54:15 +0100

    Update adb_system_updates.ps1

--- android/adb_system_updates.ps1
+++ android/adb_system_updates.ps1
@@ -1,5 +1,11 @@
-pm disable-user --user 0 com.sec.android.soagent
-pm disable-user --user 0 com.wssyncmldm
+adb devices
 
-pm enable --user 0 com.sec.android.soagent
-pm enable --user 0 com.wssyncmldm
+adb -d shell pm list packages
+
+adb -d shell pm disable-user --user 0 com.sec.android.soagent
+adb -d shell pm disable-user --user 0 com.wssyncmldm
+
+adb -d shell pm enable --user 0 com.sec.android.soagent
+adb -d shell pm enable --user 0 com.wssyncmldm
+
+adb kill-server
